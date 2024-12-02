4de736ffb7ad57ec43aee2fb4b151a314965fc26
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-02 11:20:42 +0300

    check if plate is alphanum

--- vanity_plates.py
+++ vanity_plates.py
@@ -9,7 +9,7 @@ class Plates:
             bool: _True if the length of the plate is greater than one and less than seven_
         """
         return len(plate) > 1 and len(plate) < 7
-    def starts_with_two_letters(self, plate: str) ->bool:
+    def starts_with_two_letters(self, plate: str)-> bool:
         """_A function to check if the first two characters of the plate are letter_
 
         Args:
@@ -19,3 +19,16 @@ class Plates:
             bool: _True if first two characters letters else False_
         """
         return plate[0:2].isalpha()
+    def is_plate_alphanum(plate: str)->bool:
+        """_Check if a plate contains both letters and numbers_
+
+        Args:
+            plate (str): _Requested plate_
+
+        Returns:
+            bool: _True if plate is alpha numerical otherwise False_
+        """
+        return any(char.isalpha() for char in plate)  and any(char.isnum() for char in plate)
+    def is_int_in_mid(self, plate: str)-> bool:
+        pass
+        
