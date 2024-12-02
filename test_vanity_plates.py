ebcf60b6940857abd721e3368589e593fd3929a7
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-02 12:55:00 +0300

    Testing is_valid

--- test_vanity_plates.py
+++ test_vanity_plates.py
@@ -69,3 +69,20 @@ class TestContainsPunctuation:
     plates = Plates()
     def test_contains_no_punctuation(self, plate, is_valid):
         assert self.plates.contains_no_punctuation(plate) == is_valid
+
+#Test if the plate is valid
+
+@pytest.mark.parametrize("plate, is_valid", [
+    ("CS50", True),
+    ("CS05", False),
+    ("CS50P", False),
+    ("PI3.14", False),
+    ("H", False),
+    ("OUTATIME", False),
+    ("22222", False),
+    ("HH", True)
+])
+class TestIsValid:
+    plates = Plates()
+    def test_is_valid(self, plate, is_valid):
+        assert self.plates.is_valid(plate) == is_valid
