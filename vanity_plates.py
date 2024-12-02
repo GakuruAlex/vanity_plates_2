f9a4f21d6b26b7ebbcacc2b791aaa6751c2a1fa9
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-02 10:45:01 +0300

    function to check whether the first two characters are letters

--- vanity_plates.py
+++ vanity_plates.py
@@ -9,3 +9,12 @@ class Plates:
             bool: _True if the length of the plate is greater than one and less than seven_
         """
         return len(plate) > 1 and len(plate) < 7
+    def starts_with_two_letters(plate: str) ->bool:
+        """_A function to check if the first two characters of the plate are letter_
+
+        Args:
+            plate (str): _description_
+
+        Returns:
+            bool: _description_
+        """
